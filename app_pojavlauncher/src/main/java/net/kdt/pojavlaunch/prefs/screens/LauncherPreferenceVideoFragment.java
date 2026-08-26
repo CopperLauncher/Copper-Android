48acdf9e5904fef6f69d9e06ee5453e38910aaf5
Mahdin4sure <183899034+Mahdin4sure@users.noreply.github.com>
2026-08-26 21:50:52 +0600

    feat(video): add runtime manager and runtime selection - For some reason, CodeOSS doesn't auto import - Added "Runtime Manager" in video settings - Linked MG renderer settings into the "Runtime Manager"

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 
 import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.SwitchPreference;
 import androidx.preference.SwitchPreferenceCompat;
 
@@ -23,10 +24,15 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
         int resolution = (int) (LauncherPreferences.PREF_SCALE_FACTOR * 100);
 
         //Disable notch checking behavior on android 8.1 and below.
-        requirePreference("ignoreNotch").setVisible(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && LauncherPreferences.PREF_NOTCH_SIZE > 0);
+        requirePreference("ignoreNotch").setVisible(
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.P &&
+            LauncherPreferences.PREF_NOTCH_SIZE > 0
+        );
 
-        CustomSeekBarPreference resolutionSeekbar = requirePreference("resolutionRatio",
-                CustomSeekBarPreference.class);
+        CustomSeekBarPreference resolutionSeekbar = requirePreference(
+            "resolutionRatio",
+            CustomSeekBarPreference.class
+        );
         resolutionSeekbar.setSuffix(" %");
 
         // #724 bug fix
@@ -37,13 +43,35 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
         }
 
         // Sustained performance is only available since Nougat
-        SwitchPreference sustainedPerfSwitch = requirePreference("sustainedPerformance",
-                SwitchPreference.class);
-        sustainedPerfSwitch.setVisible(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N);
-        sustainedPerfSwitch.setChecked(LauncherPreferences.PREF_SUSTAINED_PERFORMANCE);
+        SwitchPreference sustainedPerfSwitch = requirePreference(
+            "sustainedPerformance",
+            SwitchPreference.class
+        );
+        sustainedPerfSwitch.setVisible(
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+        );
+        sustainedPerfSwitch.setChecked(
+            LauncherPreferences.PREF_SUSTAINED_PERFORMANCE
+        );
 
-        requirePreference("alternate_surface", SwitchPreferenceCompat.class).setChecked(LauncherPreferences.PREF_USE_ALTERNATE_SURFACE);
-        requirePreference("force_vsync", SwitchPreferenceCompat.class).setChecked(LauncherPreferences.PREF_FORCE_VSYNC);
+        requirePreference(
+            "alternate_surface",
+            SwitchPreferenceCompat.class
+        ).setChecked(LauncherPreferences.PREF_USE_ALTERNATE_SURFACE);
+        requirePreference("force_vsync", SwitchPreferenceCompat.class).setChecked(
+            LauncherPreferences.PREF_FORCE_VSYNC
+        );
+        ListPreference rendererListPreference = requirePreference(
+            "renderer",
+            ListPreference.class
+        );
+        Tools.RenderersList renderersList = Tools.getCompatibleRenderers(
+            getContext()
+        );
+        rendererListPreference.setEntries(renderersList.rendererDisplayNames);
+        rendererListPreference.setEntryValues(
+            renderersList.rendererIds.toArray(new String[0])
+        );
 
         computeVisibility();
     }
@@ -54,8 +82,19 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
         computeVisibility();
     }
 
-    private void computeVisibility(){
-        requirePreference("force_vsync", SwitchPreferenceCompat.class)
-                .setVisible(LauncherPreferences.PREF_USE_ALTERNATE_SURFACE);
+    private void computeVisibility() {
+        requirePreference("force_vsync", SwitchPreferenceCompat.class).setVisible(
+            LauncherPreferences.PREF_USE_ALTERNATE_SURFACE
+        );
+        String currentRenderer = LauncherPreferences.DEFAULT_PREF.getString(
+            "renderer",
+            "opengles2"
+        );
+        boolean isMobileGluesRenderer = "opengles_mobileglues".equals(
+            currentRenderer
+        );
+        requirePreference("renderer_settings", Preference.class).setVisible(
+            isMobileGluesRenderer
+        );
     }
 }
